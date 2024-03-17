f218011bc9db8d5b50e20ec72bb1dc7b78898c66
ShutnyaraIs1 <147231647+ShutnyaraIs1@users.noreply.github.com>
2024-03-17 12:53:57 +0000

    Начало положено! Сделан фундамент для срезовой!

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+def load_news():
+    news = []
+    df = pd.read_excel('news.xlsx')
+    for index, row in df.iterrows():
+        news.append({
+            'title': row['Title'],
+            'date': row['Date'].strftime('%d-%m-%Y'),
+            'content': row['Content']
+        })
+    return news
+
+@app.route('/')
+def index():
+    news_list = load_news()
+    return render_template('index.html', news=news_list)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
